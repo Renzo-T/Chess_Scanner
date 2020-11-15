8d78bf82375f37ccfe66a63687fe7b0febcb2086
Reto0 <45675750+Reto0@users.noreply.github.com>
2020-11-15 16:25:49 -0600

    update working
    need to at nn now

--- ChessLocalization.py
+++ ChessLocalization.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import os
 
-def cropBorder(img, lines):
+def cropBorder(img, lines, type):
     dims = img.shape
     minX = float('inf')
     maxX = float('-inf')
@@ -26,14 +26,15 @@ def cropBorder(img, lines):
                 maxY = y1
             if(y2 > maxY):
                 maxY = y2 
-    if(minY > 10): 
-        minY = minY-10
-    if(minX > 10): 
-        minX = minX-10
-    if(dims[1] > maxX +10): 
-        maxX = maxX + 10
-    if(dims[0] > maxY +10): 
-        maxY = maxY + 10
+    # if(type == 0 or type == 1):
+    #     if(minY > 1): 
+    #         minY = minY-1
+    #     if(minX > 1): 
+    #         minX = minX-1
+    #     if(dims[1] > maxX + 1): 
+    #         maxX = maxX + 1
+    #     if(dims[0] > maxY + 1): 
+    #         maxY = maxY + 1
     crop = img[minY:maxY, minX:maxX]
     return crop
 
@@ -52,45 +53,48 @@ def localization(img):
     dims = gray.shape
     # crop_r = img
     # crop_img = edges
-    crop_r = img[100:dims[0]-40,500:dims[1]-570]
-    crop_img = gray[100:dims[0]-40,500:dims[1]-570]
+    crop_r = img[130:dims[0]-40,500:dims[1]-590]
+    crop_img = gray[130:dims[0]-40,500:dims[1]-590]
     
-    edges = cv2.Canny(crop_img,150,200)
-    cv2.imshow("gray",gray)
-    cv2.imshow("edges",edges)
+    edges = cv2.Canny(crop_img,80,200)
+    # cv2.imshow("gray",gray)
+    # cv2.imshow("edges",edges)
     # edges = cv2.Canny(crop_img,0,50,apertureSize = 3)
     # edges = cv2.Canny(crop_img,100,200)
 
 
     lines = cv2.HoughLinesP(edges,1,np.pi/180,15,50,20,20)
-    for line in lines:
-        for x1,y1,x2,y2 in line:
-            cv2.line(crop_r,(x1,y1),(x2,y2),(255,0,0),5)
+    # for line in lines:
+    #     for x1,y1,x2,y2 in line:
+    #         cv2.line(crop_r,(x1,y1),(x2,y2),(255,0,0),2)
     cannyPath = os.path.join(scriptDir, 'images/canny.png')
     cv2.imwrite(cannyPath, edges)
     crop_r_path = os.path.join(scriptDir, 'images/crop_r.png')
-    cv2.imwrite(crop_r_path, edges)
+    cv2.imwrite(crop_r_path, crop_r)
 
-    cv2.imshow("temp-1", img)
-    cv2.imshow("temp",crop_img)
-    cv2.imshow("temp2", crop_r)
-    cv2.waitKey(0)
+    # cv2.imshow("temp-1", img)
+    # cv2.imshow("temp",crop_img)
+    # cv2.imshow("temp2", crop_r)
+    # cv2.waitKey(0)
     # return crop_r
 
-    crop = cropBorder(crop_r, lines)
+    crop = cropBorder(crop_r, lines, 0)
     dims = crop.shape
     crop = crop[10:dims[0]-10,10:dims[1]-10]
-
-    edges = cv2.Canny(crop,50,150,apertureSize = 3)
-
-    lines = cv2.HoughLinesP(edges,1,np.pi/180,15,50,300,20)
+    img_blur = cv2.GaussianBlur(crop,(7,7),0)
+    gray = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray,50,150,apertureSize = 3)
+    cv2.imshow("edge2", edges)
+    lines = cv2.HoughLinesP(edges,1,np.pi/180,15,50,50,10)
+    # lines = cv2.HoughLinesP(edges,1,np.pi/180,15,50,300,20)
     # for line in lines:
-        # for x1,y1,x2,y2 in line:
-            # cv2.line(crop,(x1,y1),(x2,y2),(255,0,0),5)
-    crop2 = cropBorder(crop, lines)
-    cv2.imshow("1", crop)
-    cv2.imshow("2", crop2)
-    cv2.waitKey(0)
+    #     for x1,y1,x2,y2 in line:
+    #         cv2.line(crop,(x1,y1),(x2,y2),(255,0,0),1)
+    crop2 = cropBorder(crop, lines, 1)
+    # cv2.imshow("1", crop)
+    # cv2.imshow("2", crop2)
+    # cv2.waitKey(0)
+    cv2.destroyAllWindows()
     return crop2
 
 
--- ChessVision.py
+++ ChessVision.py
@@ -6,16 +6,28 @@ from GPCam import gpcam
 import os
 
 def main():
-    # scriptDir = os.path.dirname(__file__)
-    # img_path = os.path.join(scriptDir, 'images/frame.png')
-    # frame = cv2.imread(img_path)
+    scriptDir = os.path.dirname(__file__)
 
-    frame = gpcam()
-    # frame_path = os.path.join(scriptDir, 'images/frame.png')
-    # cv2.imwrite(frame_path, frame)
+    img_path = os.path.join(scriptDir, 'images/full.png')
+    frame = cv2.imread(img_path)
 
+    # frame = gpcam()
     board = localization(frame)
-    # img = dice(board)
+    # frame_path = os.path.join(scriptDir, 'images/full.png')
+    # cv2.imwrite(frame_path, frame)
+    while(1):
+        cv2.imshow('frame',board)
+        k = cv2.waitKey(1) & 0xFF
+        if k == ord('q'):
+            cv2.destroyAllWindows()
+            quit()
+        elif k == ord('r'):
+            frame = gpcam()
+            board = localization(frame)
+        elif k == ord('c'):
+            break
+
+    img = dice(board)
 
 if __name__ == "__main__":
     main()
