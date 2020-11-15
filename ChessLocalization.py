f87fd6a67d8453ff19119a832279f673402ea549
Reto0 <rteruya@utexas.edu>
2020-11-15 14:38:36 -0600

    revised board detection

--- ChessLocalization.py
+++ ChessLocalization.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 
 def cropBorder(img, lines):
+    dims = img.shape
     minX = float('inf')
     maxX = float('-inf')
     minY = float('inf')
@@ -25,7 +26,14 @@ def cropBorder(img, lines):
                 maxY = y1
             if(y2 > maxY):
                 maxY = y2 
-
+    if(minY > 10): 
+        minY = minY-10
+    if(minX > 10): 
+        minX = minX-10
+    if(dims[1] > maxX +10): 
+        maxX = maxX + 10
+    if(dims[0] > maxY +10): 
+        maxY = maxY + 10
     crop = img[minY:maxY, minX:maxX]
     return crop
 
@@ -34,11 +42,10 @@ def cropBorder(img, lines):
 def localization(img):
     scriptDir = os.path.dirname(__file__)
 
-    img_blur = cv2.GaussianBlur(img,(5,5),0)
+    img_blur = cv2.GaussianBlur(img,(7,7),0)
 
     gray = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
 
-    edges = cv2.Canny(gray,100,200)
 
     # ret, thresh1 = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
     # cv2.imshow("temp", thresh1)
@@ -46,13 +53,16 @@ def localization(img):
     # crop_r = img
     # crop_img = edges
     crop_r = img[100:dims[0]-40,500:dims[1]-570]
-    crop_img = edges[100:dims[0]-40,500:dims[1]-570]
+    crop_img = gray[100:dims[0]-40,500:dims[1]-570]
     
-    
+    edges = cv2.Canny(crop_img,150,200)
+    cv2.imshow("gray",gray)
+    cv2.imshow("edges",edges)
+    # edges = cv2.Canny(crop_img,0,50,apertureSize = 3)
+    # edges = cv2.Canny(crop_img,100,200)
 
-    edges = cv2.Canny(crop_img,0,50,apertureSize = 3)
 
-    lines = cv2.HoughLinesP(edges,1,np.pi/180,15,50,100,20)
+    lines = cv2.HoughLinesP(edges,1,np.pi/180,15,50,20,20)
     for line in lines:
         for x1,y1,x2,y2 in line:
             cv2.line(crop_r,(x1,y1),(x2,y2),(255,0,0),5)
@@ -65,23 +75,23 @@ def localization(img):
     cv2.imshow("temp",crop_img)
     cv2.imshow("temp2", crop_r)
     cv2.waitKey(0)
-    return crop_r
+    # return crop_r
 
-    # crop = cropBorder(crop_r, lines)
-    # dims = crop.shape
-    # crop = crop[10:dims[0]-10,10:dims[1]-10]
+    crop = cropBorder(crop_r, lines)
+    dims = crop.shape
+    crop = crop[10:dims[0]-10,10:dims[1]-10]
 
-    # edges = cv2.Canny(crop,50,150,apertureSize = 3)
+    edges = cv2.Canny(crop,50,150,apertureSize = 3)
 
-    # lines = cv2.HoughLinesP(edges,1,np.pi/180,15,50,300,20)
-    # # for line in lines:
-    #     # for x1,y1,x2,y2 in line:
-    #         # cv2.line(crop,(x1,y1),(x2,y2),(255,0,0),5)
-    # crop2 = cropBorder(crop, lines)
-    # cv2.imshow("1", crop)
-    # cv2.imshow("2", crop2)
-    # cv2.waitKey(0)
-    # return crop2
+    lines = cv2.HoughLinesP(edges,1,np.pi/180,15,50,300,20)
+    # for line in lines:
+        # for x1,y1,x2,y2 in line:
+            # cv2.line(crop,(x1,y1),(x2,y2),(255,0,0),5)
+    crop2 = cropBorder(crop, lines)
+    cv2.imshow("1", crop)
+    cv2.imshow("2", crop2)
+    cv2.waitKey(0)
+    return crop2
 
 
 def dice(img):
